6488626a07025fa97b8a39dc62c27091c50eb962
Stefan Church <stefan.church@gmail.com>
2021-09-18 19:41:59 +1200

    Add mapview

--- app/src/main/java/com/stefanchurch/ferryservices/detail/DetailFragment.kt
+++ app/src/main/java/com/stefanchurch/ferryservices/detail/DetailFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
@@ -15,23 +19,31 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
 import com.stefanchurch.ferryservices.ServicesRepository
 import com.stefanchurch.ferryservices.R
 import com.stefanchurch.ferryservices.SharedPreferences
 import com.stefanchurch.ferryservices.databinding.DetailFragmentBinding
 import com.stefanchurch.ferryservices.models.Status
 import com.stefanchurch.ferryservices.models.status
+import com.stefanchurch.ferryservices.rememberMapViewWithLifecycle
+import com.google.maps.android.ktx.awaitMap
+import com.stefanchurch.ferryservices.models.Location
+import io.sentry.Sentry
 
 class DetailFragment : Fragment() {
 
     private val args: DetailFragmentArgs by navArgs()
-//    private var mapView: MapView? = null
-//    private var map: GoogleMap? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -50,21 +62,6 @@ class DetailFragment : Fragment() {
             DetailScreen()
         }
 
-//        model.configureMap = { service ->
-//            configureMap(service)
-//        }
-//
-//        mapView = binding.mapView
-//        mapView?.onCreate(null)
-//        mapView?.getMapAsync {
-//            map = it
-//            model.service?.let { service ->
-//                configureMap(service)
-//            }
-//        }
-//
-//        model.configureView()
-
         return binding.root
     }
 
@@ -82,7 +79,32 @@ class DetailFragment : Fragment() {
         )
     ) {
         viewModel.service.value?.let { service ->
-            Column(modifier = Modifier.padding(all = 10.dp)) {
+            Column(modifier = Modifier.padding(all = 20.dp)) {
+                Box(Modifier.height(200.dp).fillMaxWidth()) {
+                    LocationsMapView(
+                        locations = service.locations,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight()
+                    )
+                    Spacer(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight()
+                            .clickable {
+                                val navController = view?.findNavController()
+                                if (navController?.currentDestination?.id == R.id.detailFragment) {
+                                    val direction = DetailFragmentDirections.actionDetailFragmentToMap(
+                                        service = service,
+                                        title = service.route
+                                    )
+                                    navController.navigate(direction)
+                                }
+                            }
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(20.dp))
                 Text(
                     text = service.area,
                     fontSize = 30.sp,
@@ -146,7 +168,7 @@ class DetailFragment : Fragment() {
                         val navController = view?.findNavController()
                         if (navController?.currentDestination?.id == R.id.detailFragment) {
                             val direction = DetailFragmentDirections.actionDetailFragmentToAdditional(service)
-                            navController?.navigate(direction)
+                            navController.navigate(direction)
                         }
                     },
                     modifier = Modifier.fillMaxWidth(),
@@ -163,57 +185,57 @@ class DetailFragment : Fragment() {
         }
     }
 
-//    private fun configureMap(service: Service) {
-//        if (service.locations.count() == 0) return
-//
-//        val map = map?.let { it } ?: return
-//
-//        map.uiSettings.setAllGesturesEnabled(false)
-//        map.setOnMarkerClickListener { true }
-//
-//        val markers = service.locations.map { location ->
-//            MarkerOptions()
-//                .position(LatLng(location.latitude, location.longitude))
-//                .title(location.name)
-//        }
-//
-//        val builder = LatLngBounds.Builder()
-//        markers.forEach {
-//            map.addMarker(it)
-//            builder.include(it.position)
-//        }
-//
-//        try {
-//            map.moveCamera(
-//                CameraUpdateFactory.newLatLngBounds(builder.build(), 90)
-//            )
-//        } catch (exception: Throwable) {
-//            Sentry.captureException(exception)
-//        }
-//    }
-//
-//    override fun onResume() {
-//        super.onResume()
-//
-//        mapView?.onResume()
-//        model.getSubscribedStatus()
-//    }
-//
-//    override fun onPause() {
-//        super.onPause()
-//
-//        mapView?.onPause()
-//    }
-//
-//    override fun onDestroy() {
-//        super.onDestroy()
-//
-//        mapView?.onDestroy()
-//    }
-//
-//    override fun onLowMemory() {
-//        super.onLowMemory()
-//
-//        mapView?.onLowMemory()
-//    }
+    @Composable
+    private fun LocationsMapView(locations: Array<Location>, modifier: Modifier) {
+        // The MapView lifecycle is handled by this composable. As the MapView also needs to be updated
+        // with input from Compose UI, those updates are encapsulated into the MapViewContainer
+        // composable. In this way, when an update to the MapView happens, this composable won't
+        // recompose and the MapView won't need to be recreated.
+        val mapView = rememberMapViewWithLifecycle()
+        MapViewContainer(mapView, locations, modifier)
+    }
+
+    @Composable
+    private fun MapViewContainer(
+        map: MapView,
+        locations: Array<Location>,
+        modifier: Modifier
+    ) {
+        val mapData = remember(locations) {
+            val markers = locations.map { location ->
+            MarkerOptions()
+                .position(LatLng(location.latitude, location.longitude))
+                .title(location.name)
+            }
+
+            val builder = LatLngBounds.Builder()
+            markers.forEach {
+                builder.include(it.position)
+            }
+
+            Pair(markers, builder.build())
+        }
+
+        LaunchedEffect(map) {
+            val googleMap = map.awaitMap()
+
+            googleMap.uiSettings.setAllGesturesEnabled(false)
+            googleMap.setOnMarkerClickListener { true }
+
+            val (markers, mapBounds) = mapData
+            markers.forEach {
+                googleMap.addMarker(it)
+            }
+
+            try {
+                googleMap.moveCamera(
+                    CameraUpdateFactory.newLatLngBounds(mapBounds, 90)
+                )
+            } catch (exception: Throwable) {
+                Sentry.captureException(exception)
+            }
+        }
+
+        AndroidView( { map }, modifier = modifier)
+    }
 }
