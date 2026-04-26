f53d0f5c9b171cbf48f0efb243fb7a0073367364
Stefan Church <stefan.church@gmail.com>
2026-04-26 21:17:18 +1200

    Shrink release builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,7 +87,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
